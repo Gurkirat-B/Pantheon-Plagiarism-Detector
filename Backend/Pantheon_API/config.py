cdb10d794b512c6cec226264595e3ecf17872e4f
Krish Patel <krishp2312@gmail.com>
2026-04-02 14:27:30 -0400

    fixed security issues

--- Backend/Pantheon_API/config.py
+++ Backend/Pantheon_API/config.py
@@ -3,24 +3,18 @@
 # ================================================
 
 import os
+from dotenv import load_dotenv
 
-#DB_HOST = os.getenv("PANTHEON_DB_HOST")
-#DB_PORT = os.getenv("PANTHEON_DB_PORT", "5432")
-#DB_NAME = os.getenv("PANTHEON_DB_NAME")
-#DB_USER = os.getenv("PANTHEON_DB_USER")
-#DB_PASS = os.getenv("PANTHEON_DB_PASSWORD")
+load_dotenv()
 
-DB_HOST = "pantheon-db.c3eceugucfdv.us-east-2.rds.amazonaws.com"
-DB_PORT = "5432"
-DB_NAME = "Pantheon_db"
-DB_USER = "Pantheon"
-DB_PASS = "pantheon11!"
+DB_HOST = os.getenv("PANTHEON_DB_HOST")
+DB_PORT = os.getenv("PANTHEON_DB_PORT", "5432")
+DB_NAME = os.getenv("PANTHEON_DB_NAME")
+DB_USER = os.getenv("PANTHEON_DB_USER")
+DB_PASS = os.getenv("PANTHEON_DB_PASSWORD")
 
+S3_BUCKET = os.getenv("PANTHEON_S3_BUCKET")
 
-#S3_BUCKET = os.getenv("PANTHEON_S3_BUCKET")
-S3_BUCKET = "pantheon-submissions"
-
-#JWT_SECRET_KEY = os.getenv("PANTHEON_JWT_SECRET_KEY", "change_this_secret_key")
-JWT_SECRET_KEY = "secretkey"
+JWT_SECRET_KEY = os.getenv("PANTHEON_JWT_SECRET_KEY", "pantheon_secret_key")
 JWT_ALGORITHM = "HS256"
 JWT_EXPIRE_MINS = 60
