9c9a021e541a1a33f6f01d907df321545896aafa
Krish Patel <krishp2312@gmail.com>
2026-04-08 22:26:31 -0400

    security update

--- Backend/Pantheon_API/config.py
+++ Backend/Pantheon_API/config.py
@@ -1,15 +1,16 @@
 # ================================================
 # config for database connection and JWT settings
 # ================================================
+import os
 
-DB_HOST = "pantheon-db.c3eceugucfdv.us-east-2.rds.amazonaws.com"
-DB_PORT = "5432"
-DB_NAME = "Pantheon_db"
-DB_USER = "Pantheon"
-DB_PASS = "pantheon11!"
+DB_HOST = os.environ["PANTHEON_DB_HOST"]
+DB_PORT = os.environ["PANTHEON_DB_PORT"]
+DB_NAME = os.environ["PANTHEON_DB_NAME"]
+DB_USER = os.environ["PANTHEON_DB_USER"]
+DB_PASS = os.environ["PANTHEON_DB_PASSWORD"]
 
-S3_BUCKET = "pantheon-submissions"
+S3_BUCKET = os.environ["PANTHEON_S3_BUCKET"]
 
-JWT_SECRET_KEY = "secretkey"
+JWT_SECRET_KEY = os.environ["PANTHEON_JWT_SECRET_KEY"]
 JWT_ALGORITHM = "HS256"
 JWT_EXPIRE_MINS = 60
